100b0bcf04cb78a69a0415a29f1a2093c090f647
Michael Lenghel <michael.lenghel@gmail.com>
2018-08-14 21:32:15 +0100

    Prints three closest results

--- dictionary.py
+++ dictionary.py
@@ -3,6 +3,7 @@ from levenshtein_algo import lev
 class Dictionary:
 	def __init__(self, word):
 		self.word = word
+		self.max_words = 3
 		with open('words.txt', 'r') as f:
 			f_contents = f.readlines()  # If a very large file, would be better to use a yeild function
 			self.find_matches(f_contents)
@@ -11,19 +12,23 @@ class Dictionary:
 		# Set the initial min_dis to the distance for first word
 		l = lev()
 
-		min_dis = l.edit_distance(self.word, f_contents[0])
-		print(f_contents[0], f_contents[1], f_contents[2],f_contents[3],f_contents[4],f_contents[5])
+		# List with 5 values, as will return 5 closest words to what the user has entered
+		min_dis = [l.edit_distance(self.word, f_contents[0])] * self.max_words
+		closest_word = [None] * self.max_words
 		dis = 0
-		closest_word = None
+	
 		for w in f_contents:
 			dis = l.edit_distance(self.word, w)
 
-			if(dis < min_dis):
-				
-				min_dis = dis
-				closest_word = w
-
-		print("Did you mean {}?".format(closest_word, min_dis))
+			for i in range(self.max_words):
+				if dis < min_dis[i]:		
+					min_dis[i] = dis
+					closest_word[i] = w
+					break
+		
+		print("Macthes found: ")
+		for i, w in enumerate(closest_word):
+			print("{}. {}".format(i, w))
 
 
 			
@@ -33,4 +38,5 @@ def main():
 	d = Dictionary(needle)
 
 
-main()
+if __name__ == '__main__':
+	main()
