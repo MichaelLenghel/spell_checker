ff2811c8021eeede6bee3cf2eaf57b7d1117c461
Michael Lenghel <michael.lenghel@gmail.com>
2018-08-14 17:53:22 +0100

    readfiles in way that properly separated for checking

--- dictionary.py
+++ dictionary.py
@@ -4,22 +4,26 @@ class Dictionary:
 	def __init__(self, word):
 		self.word = word
 		with open('words.txt', 'r') as f:
-			f_contents = f.read()  # If a very large file, f.readlines() or f.readline() is far more appropriate
+			f_contents = f.readlines()  # If a very large file, would be better to use a yeild function
 			self.find_matches(f_contents)
 
 	def find_matches(self, f_contents):
 		# Set the initial min_dis to the distance for first word
 		l = lev()
+
 		min_dis = l.edit_distance(self.word, f_contents[0])
+		print(f_contents[0], f_contents[1], f_contents[2],f_contents[3],f_contents[4],f_contents[5])
 		dis = 0
 		closest_word = None
 		for w in f_contents:
 			dis = l.edit_distance(self.word, w)
+
 			if(dis < min_dis):
+				
 				min_dis = dis
 				closest_word = w
 
-		print("The minimum word is: {} with a distance of: {}".format(closest_word, min_dis))
+		print("Did you mean {}?".format(closest_word, min_dis))
 
 
 			
